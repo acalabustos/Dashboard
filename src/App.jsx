92c138450c765667975d9d93c0a38373b3c07254
acalabustos <acalabustos@gmail.com>
2023-02-14 15:27:15 +0100

    addnewchange

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from 'react-bootstrap'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min/.css';
+
+function App () {
+    return (
+ <div className='App'>
+    <Navbar/>
+ </div>
+    )
+}
+export default App 
