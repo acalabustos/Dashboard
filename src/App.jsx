cfbf6f13964fe26b70322070b9af223846a66c09
acalabustos <acalabustos@gmail.com>
2023-02-18 23:10:35 +0100

    Finish form

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Navbar } from 'react-bootstrap'
 import './App.css'
 import './Pages/Create'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Formulario from './Pages/Create';
+import FormUpload from './Pages/Form';
 import FirstNavbar from './Pages/Navbar';
 
 function App() {
    return (
       <div className='App'>
          <FirstNavbar></FirstNavbar>
-          <Formulario></Formulario> 
+          <FormUpload></FormUpload>
 
       </div>
    );
--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -4,6 +4,11 @@ import { Form, Button } from 'react-bootstrap';
 function FormUpload() {
   const [previewImage, setPreviewImage] = useState(null);
   const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [brand, setBrand] = useState('');
+  const [condition, setCondition] = useState('');
+  const [price, setPrice] = useState('');
 
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
@@ -20,7 +25,21 @@ function FormUpload() {
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
-
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+  const handleBrandChange = (event) => {
+    setBrand(event.target.value);
+  };
+  const handleConditionChange = (event) => {
+    setCondition(event.target.value);
+  };
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
   const handleSubmit = (event) => {
     event.prevetDefault();
   }
@@ -42,38 +61,63 @@ function FormUpload() {
 
       <Form.Group className='mb-3'>
         <Form.Label htmlFor="textInput">Title:</Form.Label>
-        <Form.Control id="textInput" placeholder="Add title" />
+        <Form.Control 
+        id="textInput"  
+        value={title} 
+        onChange={handleTitleChange} 
+        placeholder="Add title" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label htmlFor="textInput">Description:</Form.Label>
-        <Form.Control id="textInput" placeholder="Add description" />
+        <Form.Control 
+        id="textInput" 
+        value={description} 
+        onChange={handleDescriptionChange}
+        placeholder="Add description" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label htmlFor="textInput">Category:</Form.Label>
-        <Form.Control id="textInput" placeholder="Add category" />
+        <Form.Control 
+        id="textInput" 
+        value={category} 
+        onChange={handleCategoryChange}
+        placeholder="Add category" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label htmlFor="textInput">Brand:</Form.Label>
-        <Form.Control id="textInput" placeholder="Add brand" />
+        <Form.Control 
+        id="textInput" 
+        value={brand} 
+        onChange={handleBrandChange}
+        placeholder="Add brand" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label htmlFor="select">Condition:</Form.Label>
-        <Form.Select id="select">
-        <option></option>
-          <option>New</option>
-          <option>Nearly new</option>
-          <option>Good</option>
-          <option>Satisfactory</option>
+        <Form.Select 
+        id="select"
+        name= "condition"
+        value={condition}
+        onChange={handleConditionChange}>
+        <option>Select option</option>
+          <option value={"new"}>New</option>
+          <option value={"nearly new"}>Nearly new</option>
+          <option value={"good"}>Good</option>
+          <option value={"satisfactory"}>Satisfactory</option>
         </Form.Select>
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label htmlFor="numberInput">Price:</Form.Label>
-        <Form.Control id="numberInput" placeholder="Add price" />
+        <Form.Control 
+        id="numberInput" 
+        value={price}
+        pattern="[0-9,.]*"
+        onChange={handlePriceChange}
+        placeholder="Add price" />
       </Form.Group>
 
 
