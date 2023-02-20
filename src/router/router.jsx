6859f0db541cb8062dccb9a40e00105799e7637d
Anyi79 <ajfr79@gmail.com>
2023-02-20 20:33:51 +0100

    add new change

---
+++ src/router/router.jsx
@@ -0,0 +1,53 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+import Dashboard from "../Pages/ProductList";
+import ProductInfo from "../components/ProductInfo";
+import Home from "../pages/Home";
+import EditProduct from "../pages/EditProduct";
+
+import { productHandler } from "../handlers/productHandler";
+import CreateProduct from "../pages/CreateProduct";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "products",
+                element: <Dashboard />,
+                loader: fetchProducts,
+            },
+            {
+                path: "products/products/:id",
+                element: <ProductInfo />,
+                loader: fetchProduct,
+            },
+            {
+                path: "newProduct",
+                element: <CreateProduct />,
+            },
+            {
+                path: "homepage",
+                element: <Home />,
+            },
+            {
+                path: "editProduct/:id",
+                element: <EditProduct />,
+                loader: fetchProduct
+            },
+
+        ],
+    },
+]);
+
+async function fetchProducts() {
+    const products = await productHandler.loadProducts();
+    return { products };
+}
+
+async function fetchProduct({ params }) {
+    const product = await productHandler.loadProduct(params.id);
+    return { product };
+}
