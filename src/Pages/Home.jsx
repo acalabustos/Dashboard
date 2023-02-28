6a68d9abd55ea51932db9dcee9843ea088372499
acalabustos <acalabustos@gmail.com>
2023-02-28 17:06:42 +0100

    structure home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../Style/Home.css";
-
+import Accordion from '../components/Accordion';
+import Carousel from '../components/Carousel';
 function Home() {
-  return (
-    <div>
-      <Container fluid>
-       <div className="prueba">
-        <Row>
-          <Col md={6}>
-            <h1>Bienvenido a mi sitio web</h1>
-            <p>Este es el texto principal de la página de inicio.</p>
-          </Col>
-          <Col md={6}>
-            <img
-              src="https://via.placeholder.com/400x300"
-              alt="Imagen de ejemplo"
-              className="img-fluid"
-            />
-          </Col>
-        </Row>
+    return (
+        <div>
+            <Container fluid>
+                <div className="prueba">
+                    <Row>
+
+                        <h1>Bienvenido a mi sitio web</h1>
+                        <p>Este es el texto principal de la página de inicio.</p>
+                        <Carousel></Carousel>
+
+                        <Accordion defaultActiveKey="0">
+                    
+                        </Accordion>
+
+                    </Row>
+                </div>
+            </Container>
         </div>
-      </Container>
-    </div>
-  );
+    );
 }
 
 export default Home;
