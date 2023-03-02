e8637b1e5ac2e60b4a42df2136558b2049524912
MariaGF <miriangimenez@gmail.com>
2023-03-02 02:49:37 +0100

    changes footes y nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../Style/FooterStyle.css';
 import { Container } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <footer className="bg-light py-3">
+    <footer className=" py-3">      
       <Container>
         <p className="text-center">&copy; {new Date().getFullYear()} Mi Empresa</p>
       </Container>
