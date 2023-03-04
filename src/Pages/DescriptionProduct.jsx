d8d38b53fa19a1dcf2bb4f1427ec80563b558ef8
acalabustos <acalabustos@gmail.com>
2023-03-04 10:33:10 +0100

    pages product

--- src/Pages/DescriptionProduct.jsx
+++ src/Pages/DescriptionProduct.jsx
@@ -1,24 +1,30 @@
-import Card from 'react-bootstrap/Card';
-import Product from '../components/Product';
-import { Link, useLoaderData } from "react-router-dom"
+import { useState, useEffect } from "react";
 
+function ProductDetail(props) {
+  const [product, setProduct] = useState(null);
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://example.com/api/products/${props.productId}`);
+      const data = await response.json();
+      setProduct(data);
+    }
+    fetchData();
+  }, [props.productId]);
 
-function CardProduct() {
   return (
-    <Card style={{ width: '28rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Favorito</Card.Link>
-        <Card.Link href="#">Compartir</Card.Link>
-      </Card.Body>
-    </Card>
+    <>
+      {product ? (
+        <div>
+          <h1>{product.name}</h1>
+          <img src={product.imageUrl} alt={product.name} />
+          <p>{product.description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
   );
 }
 
-export default CardProduct;
+export default ProductDetail;
--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,17 +1,24 @@
 import { Link, useLoaderData } from "react-router-dom"
 import Product from '../components/Product'
-
+import { Card, Button } from "react-bootstrap";
 
 
 function ProductList() {
     const { products } = useLoaderData();
+    console.log(products)
     return (
         <>
-        {products.map((product) => (
-          <Link key={product.id} to={`products/${product.id}`} name={product.name} className="product--link">
-            <Product product={product} />
-          </Link>
-          
+       {products.map((product) => (
+        <Card key={product.id} className="my-3">
+          <Card.Img variant="top" src={product.image} />
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>{product.description}</Card.Text>
+
+            <Button variant="primary"  to={`products/${product.id}`}>More Info</Button>
+            <Button variant="outline-secondary" className="mx-2">Favorite</Button>
+          </Card.Body>
+        </Card>
         ))}
       </>
     )
