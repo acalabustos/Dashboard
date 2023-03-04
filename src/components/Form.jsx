01742fbc03a48444bff994020323c415442fc68c
acalabustos <acalabustos@gmail.com>
2023-03-04 01:24:43 +0100

    merge api

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -36,9 +36,9 @@ function FormUpload() {
   const handleBrandChange = (event) => {
     setBrand(event.target.value);
   };
- /*  const handleConditionChange = (event) => {
+   const handleConditionChange = (event) => {
     setCondition(event.target.value);
-  }; */
+  }; 
   const handlePriceChange = (event) => {
     setPrice(event.target.value);
   };
@@ -102,7 +102,7 @@ function FormUpload() {
         placeholder="Add brand" />
       </Form.Group>
 
-  {/*     <Form.Group className="mb-3">
+      <Form.Group className="mb-3">
         <Form.Label htmlFor="select">Condition:</Form.Label>
         <Form.Select 
         id="select"
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import {router} from './router'
+import {router} from './Router'
 import { RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,4 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router}/>
   </React.StrictMode>
 );
-//reportWebVitals(); 
